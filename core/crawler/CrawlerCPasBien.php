b5f7e8d18f8ee498ffc2df67de7b132ae7e14dcd
remimichel <remi.michel38@gmail.com>
2014-12-13 23:15:00 +0100

    refacto some classes

--- core/crawler/CrawlerCPasBien.php
+++ core/crawler/CrawlerCPasBien.php
@@ -1,13 +1,63 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: remimichel
+ * Date: 13/12/14
+ * Time: 20:36
+ */
 
-// Inculde the phpcrawl-mainclass 
-require_once("CrawlerTrackers.php");
-require_once('libs/php-query/phpQuery.php');
+require_once("libs/php-crawl/PHPCrawler.class.php");
+require_once('libs/log4php/main/php/Logger.php');
 
+class CrawlerCPasBien extends PHPCrawler{
+    protected  $db;
+    protected  $updateData;
+    protected  $logger;
 
-// Extend the class and override the handleDocumentInfo()-method  
-class CrawlerCPasBien extends CrawlerTrackers
-{
+    private function displayReport($report){
+        if (PHP_SAPI == "cli") $lb = "\n";
+        else $lb = "<br />";
+
+        $this->logger->info("Summary:".$lb);
+        $this->logger->info("Links followed: ".$report->links_followed.$lb);
+        $this->logger->info("Documents received: ".$report->files_received.$lb);
+        $this->logger->info("Bytes received: ".$report->bytes_received." bytes".$lb);
+        $this->logger->info("Process runtime: ".$report->process_runtime." sec".$lb);
+        unlink("logs/crawler-process-id.tmp");
+    }
+
+    public static function crawlNew($baseURL, $tracker){
+        $crawler = new self();
+        error_reporting(E_ALL);
+        $crawler->setLogger("new-data", $tracker);
+        $crawler->setUrlCacheType(PHPCrawlerUrlCacheTypes::URLCACHE_SQLITE);
+        $crawler->setURL($baseURL);
+        $crawler->addContentTypeReceiveRule("#text/html#");
+        $crawler->addURLFilterRule("#\.(jpg|jpeg|gif|png|torrent|exe|css|js)$# i");
+        $crawler->enableCookieHandling(true);
+        $crawler->enableResumption();
+        (!file_exists("logs/crawler-process-id.tmp")) ? file_put_contents("logs/crawler-process-id.tmp", $crawler->getCrawlerId()) :  $crawler->resume(file_get_contents("logs/crawler-process-id.tmp"));
+        $crawler->goMultiProcessed(5);
+        $crawler->displayReport($report = $crawler->getProcessReport());
+        //$db->cpasbien->execute('ensureIndex({"slug":1}');
+    }
+
+    public static function crawlUpdate($db, $cursor, $tracker){
+        $crawler = new self();
+        $crawler->setLogger("update-data", $tracker);
+        $crawler->setUpdateData(true);
+        $crawler->setDb($db);
+        $crawler->addContentTypeReceiveRule("#text/html#");
+        $crawler->addURLFilterRule("#\.(.*)$# i");
+        $crawler->enableCookieHandling(true);
+        foreach($cursor as $obj){
+            $crawler->setURL($obj["url"]);
+            $crawler->go();
+            $report = $crawler->getProcessReport();
+            $crawler->displayReport($report);
+        }
+    }
+
     function handleDocumentInfo($DocInfo)
     {
         // Just detect linebreak for output ("\n" in CLI-mode, otherwise "<br>").
@@ -39,4 +89,51 @@ class CrawlerCPasBien extends CrawlerTrackers
         flush();
     }
 
-}
+    function initChildProcess(){
+        $mongo = new MongoClient();
+        $this->db = $mongo->selectDB("torrents");
+    }
+
+    function slugify($str, $replace=array(), $delimiter='-') {
+        if( !empty($replace) ) {
+            $str = str_replace((array)$replace, ' ', $str);
+        }
+
+        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+        $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+        $clean = strtolower(trim($clean, '-'));
+        $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+
+        return $clean;
+    }
+
+    public function setDb($db){
+        $this->db = $db;
+    }
+
+    public function setUpdateData($updateData){
+        $this->updateData = $updateData;
+    }
+
+    public function setLogger($type, $tracker){
+        $date = new DateTime();
+        Logger::configure(array(
+            'rootLogger' => array(
+                'appenders' => array('default'),
+            ),
+            'appenders' => array(
+                'default' => array(
+                    'class' => 'LoggerAppenderFile',
+                    'layout' => array(
+                        'class' => 'LoggerLayoutSimple'
+                    ),
+                    'params' => array(
+                        'file' => 'logs/' . $date->format("Ymd") . '-' . $tracker . '-' . $type . '.log',
+                        'append' => true
+                    )
+                )
+            )
+        ));
+        $this->logger = Logger::getLogger("main");
+    }
+} 
