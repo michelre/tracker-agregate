8e4f4d299b16289ed4d0b72a109b0e3fc6e596f8
remimichel <remi.michel38@gmail.com>
2014-12-21 17:23:01 +0100

    refacto crawler kickass

---
+++ core/script/kickass/new-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+DATA_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../../data/kickass && pwd)
+NB_PROCESSES=5
+
+#cleaning data directory
+#$(rm -rf $DATA_DIR/*)
+
+#Downloading and decompressing data
+#$(wget --no-check-certificate https://kickass.so/dailydump.txt.gz && gzip --uncompres *.gz && mv *.txt $DATA_DIR && rm -rf *.gz)
+
+#Splitting file
+NB_LINES=$(cat $DATA_DIR/*.txt | wc -l)
+NB_LINES_PER_PROCESS=$((NB_LINES/NB_PROCESSES))
+#$(cd $DATA_DIR && mkdir tmp && cd tmp && split -l $NB_LINES_PER_PROCESS ../*.txt)
+
+#Spawing processes
+for f in $DATA_DIR/tmp/*; do
+    php -f $SCRIPT_DIR/new-data.php $f &
+done
